a435e58aefb6a299bf55109e457264c857c9de91
Relachado <130484091+Alguns-Codigos@users.noreply.github.com>
2023-08-12 19:00:22 -0300

    12-08-2023

--- Pagina_Postar_Grupos.py
+++ Pagina_Postar_Grupos.py
@@ -49,7 +49,18 @@ def Grupos():
             R = 2
 
             chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--disable-extensions')
+            chrome_options.add_argument('--disable-software-rasterizer')
+            chrome_options.add_argument('--disable-infobars')
+            chrome_options.add_argument('--remote-debugging-port=9222')
+            chrome_options.add_argument('--disable-browser-side-navigation')
+            chrome_options.add_argument('--disable-web-security')
+            chrome_options.add_argument('--disable-popup-blocking')
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument('--disable-features=VizDisplayCompositor')
+
             driver = webdriver.Chrome(options=chrome_options)
             driver.delete_all_cookies()
             driver.set_window_size(1200, 700)
